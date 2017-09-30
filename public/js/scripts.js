c7a3aab1c7e8b9b263ca38f6f00037ab4bcde3be
Jacob Marks <jacobjmarks@outlook.com>
2017-10-01 09:28:21 +1000

    Added d3 source.
    Some kmer compare cleanup.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,12 @@
 window.onload = () => {
+    var sankey = d3.sankey();
+
+    console.time("compare");
     compare(
         "aaggccgtaatgaccggcagtgtcgacggcgccgaccggtccgggcctggtcccgcgaacttgaggggaacgatattctgctcaaaagaataatttgaatcaaccctcgcgtcaatgagtagaaagtgaatggggttgctttggagtgttgaagtggtcgtcatctgcaatcaccatttcttgtatcttcacgcccgtacaggacctaattaagatggtttgttacaagtacctgactataagtctgttaccgaggggatcgggggacacgacaggccctccctacgcgacgaggtaggtatggatcaaatagacactcaaaactcatgagtattgggtacctaattcgcctcttagttacccaccgcggggaccaccgcgaggaagccatttcagagtgacacctgggacggatattgaaggatctgccctgaccacggtacactactggggcgtctaattcgccgcaattgagagtatggcccctatcacacttttgcgatgggttcggtccggtcgtcgtaacacattcggagccgtgaaacgtcaatcggtacaggcccggtataactctgcttcttagtgctccagggaaacctcgaatctgaaaatatcatacaagcggtggcgttggccatttcacaggatggcgtatttaattacgattgccgattagtcagctcaagtatgcagtccaaagattatcgtccaaagaccaaactcgtcaatcaattagatgtagtgcagtggttctcatccgacgcgcgctctcacactgggggctgtagcccgctggtcttgaaagtgattttccgcgtctactgagagaaagatgcgtcgtgtgtagacgatgtgttccgatgcagcctttagatcctcgggaacatgtatacgtcgcatctgtgacgacgggaaaaatgcactcgttcgactcacgtaccgcgttgtatttctgtcccacagctgggccaagtcacactgtggtaggtctccagtacca",
         "tcgaccacgtcgtgagtggtgtaaaatgttggcatccgtaagcttaacatgtaatcgagcgtgagtctgcgatactcgtgcgactccgggactaatgtgaaattccatgcacgtttcgatattaatagtctgcggttctcttgtatgccccatgaagagatttcacagccgggtttctgccagccacacatgtcctacatcaacgtatagttgattaggccccacgcacttctagtttgcagttttaaaactatgttccagattagcgtttagagacgttgatttactgtcgcgtcttcacccaacgttgctctgcataactaagccttttctaaaagtgctgcgtctgatatatagagtagcgatagcgcgtccattcgtagctagatcaagtcagcaggacttagattcactgcccaagtagcaatcacccaatgaaacgggaacttagccgaagcggtactcttttatttcatagtggacccgacgtcctatccttgaaataattagtcaatatgacggaatgagaatgcttctaaacttaagatagtacgttcgtagctgagagctttattattagattgcttcgcctgccagatctgcattcaccagtctttgcatatggaagtcggtcaacgatcagggatgctgtgctcgtctaggctagaaccggggacctatgcatagtaccacaatatcggcgggggtctaatgcggatagagggacgggcctcggatccaaatacctcatctgaattcagacgtagaatggagggccccctatatgatcatatacatgcgacatctcgcgcgaacatgctttctagtggccatagagtggcgtcctcaactatatgaaattcagcgtctccaatccctcaagcgatgtaccccataaaatgatactaaggtgaccaagccattggcctagcgacttgtttgccgcgatagactaccgacctgatgtcagcacacctgcccttcctcaccctccacgaac"
     )
+    console.timeEnd("compare");
 }
 
 const MIN_KMER = 4;
@@ -10,20 +14,19 @@ const MIN_KMER = 4;
 function compare(seqA, seqB) {
     let results = [];
 
-    let findPairs = function(seqA, seqB) {
-        let findKmers = function(seq) {
-            let kmers = [];
-            for (let kmer_len = seqA.length; kmer_len >= MIN_KMER; kmer_len--) {
-                for (let i = 0; i <= seqA.length - kmer_len; i++) {
-                    let kmer = seqA.substring(i, i + kmer_len);
-                    kmers.push(kmer);
-                }
+    let findKmers = function(seq) {
+        let kmers = [];
+        for (let kmer_len = seqA.length; kmer_len >= MIN_KMER; kmer_len--) {
+            for (let i = 0; i <= seqA.length - kmer_len; i++) {
+                let kmer = seqA.substring(i, i + kmer_len);
+                kmers.push(kmer);
             }
-            return kmers;
         }
-        let kmers = findKmers(seqA);
-        console.log(kmers.filter((kmer) => seqB.includes(kmer)));
+        return kmers;
     }
+
+    let kmers = findKmers(seqA);
+    console.log(kmers.filter((kmer) => seqB.includes(kmer)));
 }
 
 function displayResults(seqA, seqB, results) {
