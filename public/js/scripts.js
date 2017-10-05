dbae2821fc3deab20a9e0f1d822cb660f06c44ca
Jacob Marks <jacobjmarks@outlook.com>
2017-10-05 15:32:36 +1000

    Removed CircosJS; Begun D3 reform.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,34 +1,25 @@
-window.onload = () => {
-    circos = new Circos({
-        container: "#chart"
-    });
+const SEQUENCES = [];
 
+$(document).ready(() => {
+    const NUM_SEQS = 2;
     let alphabet = "ACGT";
 
-    const NUM_SEQS = 4;
+    for (let i = 0; i < NUM_SEQS; i++) {
+        let seq = "";
+        let seq_len = 100;
+        for (let j = 0; j < seq_len; j++) {
+            seq += alphabet[Math.floor(Math.random() * 4)];
+        }
+        SEQUENCES.push(seq);
+    }
 
-    compare(
-        (() => {
-            let seqs = [];
-            for (let i = 0; i < NUM_SEQS; i++) {
-                let seq = "";
-                let seq_len = Math.floor(Math.random() * 100) + 25;
-                for (let j = 0; j < seq_len; j++) {
-                    seq += alphabet[Math.floor(Math.random() * 4)];
-                }
-                seqs.push(seq);
-            }
-            return seqs;
-        })()
-    );
-}
+    drawResults(compare(SEQUENCES));
+})
 
 const MIN_KMER = 6;
 
 function commonKmers(seqA, seqB) {
     let kmers = [];
-
-    // let kmer_len = seqA.length;
 
     for (let kmer_len = seqA.length; kmer_len >= MIN_KMER; kmer_len--) {
         for (let i = 0; i <= seqA.length - kmer_len; i++) {
@@ -65,100 +56,19 @@ function compare(seqs) {
         }
     }
 
-    drawChart(seqs, matches);
+    return matches;
 }
 
-function drawChart(seqs, matches) {
-    let nodes = seqs.map((seq) => {
-        return {
-            len: seq.length,
-            id: seq,
-            label: seq
+function drawResults(matches) {
+    let chart = d3.select("#chart");
+    
+    chart
+    .selectAll("div")
+    .data(SEQUENCES)
+    .enter().each((seq) => {
+        let seq_div = chart.append("div").classed("sequence", true);
+        for (let i = 0; i < seq.length; i++) {
+            seq_div.append("l").text(seq[i]);
         }
-    });
-
-    circos
-        .layout(nodes, {
-            innerRadius: 320,
-            outerRadius: 320,
-            labels: {
-                display: false,
-                radialOffset: 85
-            },
-            ticks: {
-                display: false,
-                spacing: 10
-            }
-        })
-        .text(
-            'text',
-            seqs.map((seq) => {
-                let chars = [];
-                for (let i = 0; i < seq.length; i++) {
-                    chars.push({
-                        block_id: seq,
-                        position: i + 1,
-                        value: seq[i]
-                    });
-                }
-                return chars;
-            }).reduce((a,b) => {
-                return a.concat(b);
-            }),
-            {
-                innerRadius: 1.01,              
-                style: {
-                    "font-size": 8,
-                    "font-weight": "bold",
-                }
-            }
-        )
-        .highlight(
-            'highlight',
-            seqs.map((seq) => {
-                let chars = [];
-                for (let i = 0; i < seq.length; i++) {
-                    chars.push({
-                        block_id: seq,
-                        start: i,
-                        end: i + 1,
-                        name: seq[i]
-                    });
-                }
-                return chars;
-            }).reduce((a,b) => {
-                return a.concat(b);
-            }),
-            {
-                innerRadius: 1,
-                outerRadius: 0.9,
-                tooltipContent: (d) => {
-                    return "<p>" + d.name + "</p>";
-                },
-                color: "white",
-                strokeColor: "black",
-                strokeWidth: 1,
-            }
-        )
-        .chords(
-            'chords',
-            matches.reduce((a,b) => {
-                return a.concat(b);
-            }).map((match) => {
-                return {
-                    source: match.source,
-                    target: match.target
-                }
-            }),
-            {
-                color: () => {
-                    return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);                    
-                },
-                opacity: 0.5,
-                tooltipContent: (chord) => {
-                    return "<h3>" + chord.value + "</h3>";
-                }
-            }
-        )
-        .render();
+    })
 }
